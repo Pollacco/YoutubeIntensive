e48d674387c6fa626f554ae436c9f5323990ac16
Pollacco <andrey.sviontek@gmail.com>
2019-10-01 12:13:02 +0500

    build: add .jshintrc

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const switcher = document.querySelector('#cbx'),
+    more = document.querySelector('.more'),
+    modal = document.querySelector('.modal'),
+    videos = document.querySelector('.video__item');
+let player = document.querySelector('.');
